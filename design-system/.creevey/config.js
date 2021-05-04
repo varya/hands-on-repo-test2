84da7175cbb995d7ffdb3f019f7b294acc6d773a
Varya Stepanova <mail@varya.me>
2021-05-04 10:10:33 +0300

    Initial commit

---
+++ design-system/.creevey/config.js
@@ -0,0 +1,6 @@
+const path = require('path');
+
+module.exports = {
+  screenDir: path.join(__dirname, '../tests/referenceImages'),
+  reportDir: path.join(__dirname, '../tests/report'),
+}
---
+++ design-system/src/index.js
@@ -0,0 +1,16 @@
+/*
+ * This is an index file for your library.
+ * It's being updated automatically by add-component script
+ * Don't edit it directly, your chages will be overwritten.
+ */
+
+export { default as GlobalStyle } from "./global.js";
+export { colors, typography } from "./tokens";
+
+
+export { default as Avatar } from "./components/Avatar";
+export { default as Button } from "./components/Button";
+export { default as Carousel } from "./components/Carousel";
+export { default as Icon } from "./components/Icon";
+export { default as Select } from "./components/Select";
+export { default as SummaryTable } from "./components/SummaryTable";
